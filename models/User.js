261ed7e3d6ae7201b5c649ac4f2413d00c39332b
Lakshay <102456284+Monkfuego@users.noreply.github.com>
2025-09-19 09:50:53 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
-  password: { type: String }, // only for email/password users
-  google: { type: Boolean, default: false },
+  password: { type: String }, 
+  google: { type: Boolean, default: false }, 
   createdAt: { type: Date, default: Date.now },
 });
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
+
